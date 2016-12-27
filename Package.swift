7ef71000172c2f76fd34b6677cc763faf2833c63
nya <home@cathand.org>
2016-12-27 10:18:28 +0900

    .

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 	name: "CHAmazonProductAdvertisingAPI",
 	targets: [],
 	dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libxml2.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0),
     ],
 	exclude: []
