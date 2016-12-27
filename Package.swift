b910ab460ca3d5a94e7886326e13e2c531f28347
nya <home@cathand.org>
2016-12-27 10:01:00 +0900

    add a dependency

--- Package.swift
+++ Package.swift
@@ -8,6 +8,8 @@ import PackageDescription
 let package = Package(
 	name: "CHAmazonProductAdvertisingAPI",
 	targets: [],
-	dependencies: [],
+	dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0),
+    ],
 	exclude: []
 )
